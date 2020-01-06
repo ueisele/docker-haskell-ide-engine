2f09eb991a274548c98fa8628d1812f028369e4a
Uwe Eisele <code@uweeisele.eu>
2020-01-06 15:51:44 +0100

    completed build script

--- build.sh
+++ build.sh
@@ -5,8 +5,11 @@ cd $(dirname ${BASH_SOURCE[0]})
 ROOT_DIR=$(pwd)
 popd > /dev/null
 
+DOCKERIMAGE_REPO="ueisele/haskell-stack-hie"
+
 PUSH=false
 BUILD=false
+DOCKERFILE_DIR=""
 
 function usage () {
     echo "$0: $1" >&2
@@ -18,7 +21,6 @@ function usage () {
 }
 
 function parseCmd () {
-    STACK_RESOLVER_ARG=
     while [[ $# -gt 0 ]]; do
         case "$1" in
             --build)
@@ -30,30 +32,93 @@ function parseCmd () {
                 shift
                 ;;
             -*)
-                return usage ("Unknown option: $1")
+                usage "Unknown option: $1"
+                return $?
                 ;;
             *)
-                if [[ -n "$STACK_RESOLVER_ARG" ]]; then
-                    return usage ("Cannot specify multiple resolvers: $1")
+                if [[ -n "$DOCKERFILE_DIR" ]]; then
+                    usage "Cannot specify multiple directories: $1"
+                    return $?
                 fi
-                STACK_RESOLVER_ARG="$1"
+                DOCKERFILE_DIR="$1"
                 shift
                 ;;
         esac
     done
+    if [ -z "${DOCKERFILE_DIR}" ]; then
+        usage "Requires directory"
+        return $?
+    fi
+    if [ ! -f "${DOCKERFILE_DIR}/Dockerfile" ]; then 
+        usage "Missing Dockerfile: ${DOCKERFILE_DIR}/Dockerfile"
+    fi
     return 0
 }
 
+function buildTimestamp() {
+    date --utc -u +'%Y%m%dT%H%M%Z'
+}
+
+function resolveRepo () {
+    git config --get remote.origin.url
+}
+
+function resolveCommit () {
+    git rev-list --abbrev-commit --abbrev=7 -1 master ${DOCKERFILE_DIR}
+}
+
+function resolveImageLabel () {
+    local label=${1:-"Missing label name as first parameter!"}
+    docker inspect \
+        --format "{{ index .Config.Labels \"${label}\"}}" \
+        "${DOCKERIMAGE_REPO}:${DOCKERFILE_DIR}"
+}
+
+function resolveImageTags () {
+    local resolver=$(resolveImageLabel "stack.resolver")
+    local timestamp=$(resolveImageLabel "build.timestamp")
+    local commit=$(resolveImageLabel "source.git.commit")
+    echo "${resolver}" "${resolver}-${timestamp}-${commit}"
+}
+
+function build () {
+    pushd . > /dev/null
+    cd $ROOT_DIR
+    docker build -t ${DOCKERIMAGE_REPO}:${DOCKERFILE_DIR} \
+        -f ${DOCKERFILE_DIR}/Dockerfile \
+        --build-arg BUILD_TIMESTAMP=$(buildTimestamp) \
+        --build-arg SOURCE_GIT_REPOSITORY=$(resolveRepo) \
+        --build-arg SOURCE_GIT_COMMIT=$(resolveCommit) \
+        ${DOCKERFILE_DIR}
+    popd > /dev/null
+}
+
+function tag () {
+    for t in $(resolveImageTags); do
+        docker tag "${DOCKERIMAGE_REPO}:${DOCKERFILE_DIR}" "${DOCKERIMAGE_REPO}:${t}"
+    done
+}
+
 function push () {
-    local
-    [[ $PUSH = false ]] || docker push "$1"
+    for t in $(resolveImageTags); do
+        docker push "${DOCKERIMAGE_REPO}:${t}"
+    done
 }
 
 function main () {
-    local retval = parseCmd ()
-    if [ retval != 0 ]; then
+    parseCmd "$@"
+    local retval=$?
+    if [ $retval != 0 ]; then
         exit $retval
-    fi;
+    fi
+
+    if [ "$BUILD" = true ]; then
+        build
+        tag
+    fi
+    if [ "$PUSH" = true ]; then
+        push
+    fi
 }
 
 main "$@"
